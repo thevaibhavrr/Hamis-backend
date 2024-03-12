05d7a7174b18abf53d62042b3b873f49ec539cbd
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-03-12 12:37:47 +0530

    added cors

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cors = require("cors");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
